cf93fb6e393c3627652329f75b666b4c50801aac
GeheimagentNr1 <GeheimeLache@hotmail.com>
2020-06-07 13:36:56 +0200

    Spawn point wird jetzt bei Tag vom Bett und nicht mehr vom Right Click Event gesetzt.

--- src/main/java/de/geheimagentnr1/easier_sleeping/handlers/ForgeEventHandler.java
+++ src/main/java/de/geheimagentnr1/easier_sleeping/handlers/ForgeEventHandler.java
@@ -4,14 +4,7 @@ import de.geheimagentnr1.easier_sleeping.config.ModConfig;
 import de.geheimagentnr1.easier_sleeping.elements.commands.SleepCommand;
 import de.geheimagentnr1.easier_sleeping.sleeping.SleepingManager;
 import de.geheimagentnr1.easier_sleeping.sleeping.SleepingWorker;
-import net.minecraft.block.BedBlock;
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.world.World;
 import net.minecraftforge.common.WorldWorkerManager;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
@@ -29,22 +22,5 @@ public class ForgeEventHandler {
 		ModConfig.load();
 		SleepingManager.init();
 		WorldWorkerManager.addWorker( new SleepingWorker() );
-	}
-	
-	@SubscribeEvent
-	public static void handleRightClickBlockEvent( PlayerInteractEvent.RightClickBlock event ) {
-		
-		World world = event.getWorld();
-		PlayerEntity player = event.getPlayer();
-		
-		if( !world.isRemote() && world.isDaytime() && !player.isSpectator() && !player.isSneaking() ) {
-			BlockPos pos = event.getPos();
-			BlockState state = world.getBlockState( pos );
-			if( state.getBlock() instanceof BedBlock ) {
-				player.setSpawnPoint( pos, false, false, event.getWorld().getDimension().getType() );
-				player.sendMessage( new StringTextComponent( "Set new spawnpoint" ) );
-				event.setCanceled( true );
-			}
-		}
 	}
 }
